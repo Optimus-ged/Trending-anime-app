664739ee926e5aa52dd766cb168ebd60feb95168
Optimus ged <gedeonyalakuhanda@gmail.com>
2024-10-08 16:44:41 +0200

    some issues fixed

--- app/src/main/java/com/animeapp/animeappproject/ui/screen/anime/AnimeScreenViewModel.kt
+++ app/src/main/java/com/animeapp/animeappproject/ui/screen/anime/AnimeScreenViewModel.kt
@@ -1,13 +1,12 @@
 package com.animeapp.animeappproject.ui.screen.anime
 
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.animeapp.animeappproject.core.util.Resource
 import com.animeapp.animeappproject.domain.use_case.GetTrendingAnimeById
-import com.animeapp.animeappproject.ui.screen.trending_anime_list.TrendingListState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
@@ -18,8 +17,9 @@ class AnimeScreenViewModel @Inject constructor(
     private val getTrendingAnimeById: GetTrendingAnimeById
 ) : ViewModel(){
 
-    private val _state = mutableStateOf(TrendingAnimeState())
-    var state : State<TrendingAnimeState> = _state
+    private val _state = MutableStateFlow(TrendingAnimeState())
+    val state = _state.asStateFlow()
+
 
     fun fetchAnime(id : String){
         viewModelScope.launch {
